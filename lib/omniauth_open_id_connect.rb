0964566614d5794bfb3a1af20e41585dce5cf855
unknown <sangvn1@viettel.com.vn>
2022-08-08 15:35:26 +0700

    commit

---
+++ lib/omniauth_open_id_connect.rb
@@ -0,0 +1,222 @@
+# frozen_string_literal: true
+
+require 'omniauth-oauth2'
+
+module ::OmniAuth
+  module OpenIDConnect
+    class DiscoveryError < Error; end
+  end
+
+  module Strategies
+    class OpenIDConnect < OmniAuth::Strategies::OAuth2
+      class NonceVerifyError < StandardError; end
+
+      option :scope, "openid"
+      option :discovery, true
+      option :discovery_document, nil
+      option :use_userinfo, true
+      option :error_handler, lambda { |error, message| nil } # Default no-op handler
+      option :verbose_logger, lambda { |message| nil } # Default no-op handler
+      option :passthrough_authorize_options, [:p]
+      option :passthrough_token_options, [:p]
+      option :claims, nil
+
+      option :client_options,
+        site: nil,
+        authorize_url: nil,
+        token_url: nil,
+        userinfo_endpoint: nil,
+        auth_scheme: :basic_auth
+
+      def verbose_log(message)
+        options.verbose_logger.call(message)
+      end
+
+      def discover!
+        discovery_document = options[:discovery_document]
+        raise ::OmniAuth::OpenIDConnect::DiscoveryError.new("Discovery document is missing") if discovery_document.nil?
+
+        discovery_params = {
+          authorize_url: "authorization_endpoint",
+          token_url: "token_endpoint",
+          site: "issuer"
+        }
+
+        discovery_params.each do |internal_key, external_key|
+          val = discovery_document[external_key]
+          raise ::OmniAuth::OpenIDConnect::DiscoveryError.new("missing discovery parameter #{external_key}") if val.nil? || val.empty?
+          options[:client_options][internal_key] = val
+        end
+
+        userinfo_endpoint = options[:client_options][:userinfo_endpoint] = discovery_document["userinfo_endpoint"].to_s
+        options.use_userinfo = false if userinfo_endpoint.nil? || userinfo_endpoint.empty?
+
+        if discovery_document["token_endpoint_auth_methods_supported"] && !discovery_document["token_endpoint_auth_methods_supported"].include?("client_secret_basic") && discovery_document["token_endpoint_auth_methods_supported"].include?("client_secret_post")
+          options[:client_options][:auth_scheme] = :request_body
+        end
+      end
+
+      def request_phase
+        begin
+          discover! if options[:discovery]
+        rescue ::OmniAuth::OpenIDConnect::DiscoveryError => e
+          return fail!(:openid_connect_discovery_error, e)
+        end
+
+        super
+      end
+
+      def authorize_params
+        super.tap do |params|
+          options[:passthrough_authorize_options].each do |k|
+            params[k] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
+          end
+
+          if options[:claims].present?
+            params[:claims] = options[:claims]
+          end
+
+          params[:scope] = options[:scope]
+          session['omniauth.nonce'] = params[:nonce] = SecureRandom.hex(32)
+
+          options[:passthrough_token_options].each do |k|
+            session["omniauth.param.#{k}"] = request.params[k.to_s] unless [nil, ''].include?(request.params[k.to_s])
+          end
+        end
+      end
+
+      def token_params
+        params = {}
+        options[:passthrough_token_options].each do |k|
+          val = session.delete("omniauth.param.#{k}")
+          params[k] = val unless [nil, ''].include?(val)
+        end
+        super.merge(params)
+      end
+
+      def callback_phase
+        if request.params["error"] && request.params["error_description"] && response = options.error_handler.call(request.params["error"], request.params["error_description"])
+          verbose_log("Error handled, redirecting\n\n#{response.to_yaml}")
+          return redirect(response)
+        end
+
+        begin
+          discover! if options[:discovery]
+
+          oauth2_callback_phase = super
+          return oauth2_callback_phase if env['omniauth.error']
+
+          oauth2_callback_phase
+        rescue ::OmniAuth::OpenIDConnect::DiscoveryError => e
+          fail!(:openid_connect_discovery_error, e)
+        rescue ::JWT::DecodeError => e
+          fail!(:jwt_decode_failed, e)
+        rescue NonceVerifyError => e
+          fail!(:jwt_nonce_verify_failed, e)
+        end
+      end
+
+      def id_token_info
+        # Verify the claims in the JWT
+        # The signature does not need to be verified because the
+        # token was acquired via a direct server-server connection to the issuer
+        @id_token_info ||= begin
+          decoded = ::JWT.decode(access_token['id_token'], nil, false).first
+          verbose_log("Loaded JWT\n\n#{decoded.to_yaml}")
+          ::JWT::Verify.verify_claims(decoded,
+            verify_iss: true,
+            iss: options[:client_options][:site],
+            verify_aud: true,
+            aud: options.client_id,
+            verify_sub: false,
+            verify_expiration: true,
+            verify_not_before: true,
+            verify_iat: false,
+            verify_jti: false
+          )
+
+          if decoded["nonce"].nil? || decoded["nonce"].empty? || decoded["nonce"] != session.delete("omniauth.nonce")
+            raise NonceVerifyError.new "JWT nonce is missing, or does not match"
+          end
+
+          verbose_log("Verified JWT\n\n#{decoded.to_yaml}")
+
+          decoded
+        end
+      end
+
+      def userinfo_response
+        @raw_info ||= begin
+                        print "--------------------------------------------" + access_token.to_json
+
+          info = access_token.get(options[:client_options][:userinfo_endpoint]).parsed
+          verbose_log("Fetched userinfo response\n\n#{info.to_yaml}")
+          info
+        end
+
+        return fail!(:csrf_detected, CallbackError.new(:csrf_detected, "CSRF detected")) unless @raw_info['sub'] == id_token_info['sub']
+        @raw_info
+      end
+
+      uid { id_token_info['sub'] }
+
+      info do
+        print "----------------------------" + userinfo_response.to_json
+        data_source = options.use_userinfo ? userinfo_response : id_token_info
+        prune!(
+          # name: data_source['name'],
+          # username: data_source['userName'],
+          # email: data_source['email'],
+          # first_name: data_source['given_name'],
+          # last_name: data_source['family_name'],
+          # nickname: data_source['preferred_username'],
+          # image: data_source['picture']
+          name: data_source["attributes"]['fullName'],
+          username: data_source["attributes"]['staffCode'],
+          email: data_source["attributes"]['email'],
+          nickname: data_source["attributes"]['staffCode']
+        )
+      end
+
+      extra do
+        hash = {}
+        hash[:raw_info] = options.use_userinfo ? userinfo_response : id_token_info
+        hash[:id_token] = access_token['id_token']
+        prune! hash
+      end
+
+      private
+
+      def callback_url
+        SiteSetting.openid_connect_callback.presence ||
+          (full_host + script_name + callback_path)
+      end
+
+      def get_token_options
+        { redirect_uri: callback_url,
+          grant_type: 'authorization_code',
+          code: request.params["code"],
+          client_id: options[:client_id],
+          client_secret: options[:client_secret]
+        }.merge(token_params.to_hash(symbolize_keys: true))
+      end
+
+      def prune!(hash)
+        hash.delete_if do |_, v|
+          prune!(v) if v.is_a?(Hash)
+          v.nil? || (v.respond_to?(:empty?) && v.empty?)
+        end
+      end
+
+      protected
+
+      def build_access_token
+        return super if options.use_userinfo
+        response = client.request(:post, options[:client_options][:token_url], body: get_token_options)
+        ::OAuth2::AccessToken.from_hash(client, response.parsed)
+      end
+    end
+  end
+end
+
+OmniAuth.config.add_camelization 'openid_connect', 'OpenIDConnect'
---
+++ spec/lib/omniauth_open_id_connect_spec.rb
@@ -0,0 +1,205 @@
+# frozen_string_literal: true
+
+require_relative '../../lib/omniauth_open_id_connect'
+require 'rails_helper'
+
+describe OmniAuth::Strategies::OpenIDConnect do
+  let(:app) do
+    @app_called = false
+    lambda do |*args|
+      @app_called = true
+      [200, {}, ['Hello.']]
+    end
+  end
+
+  let(:discovery_document) do
+    {
+      "issuer" => "https://id.example.com/",
+      "authorization_endpoint" => "https://id.example.com/authorize",
+      "token_endpoint" => "https://id.example.com/token",
+      "userinfo_endpoint" => "https://id.example.com/userinfo",
+    }
+  end
+
+  subject do
+    OmniAuth::Strategies::OpenIDConnect.new(app, 'appid', 'secret', discovery_document: discovery_document)
+  end
+
+  before { OmniAuth.config.test_mode = true }
+
+  after { OmniAuth.config.test_mode = false }
+
+  it "throws error for missing discovery document" do
+    strategy = OmniAuth::Strategies::OpenIDConnect.new(app, 'appid', 'secret', discovery_document: nil)
+    expect { strategy.discover! }.to raise_error(::OmniAuth::OpenIDConnect::DiscoveryError)
+  end
+
+  it "throws error for invalid discovery document" do
+    discovery_document.delete("authorization_endpoint")
+    expect { subject.discover! }.to raise_error(::OmniAuth::OpenIDConnect::DiscoveryError)
+  end
+
+  it "disables userinfo if not included in discovery document" do
+    discovery_document.delete("userinfo_endpoint")
+    subject.discover!
+    expect(subject.options.use_userinfo).to eq(false)
+  end
+
+  it "uses basic authentication when no endpoint auth methods are provided" do
+    subject.discover!
+    expect(subject.options.client_options.auth_scheme).to eq(:basic_auth)
+  end
+
+  it "uses basic authentication when both client_secret_basic and client_secret_post are provided" do
+    discovery_document.merge!({ "token_endpoint_auth_methods_supported" => ["client_secret_basic", "client_secret_post"] })
+    subject.discover!
+    expect(subject.options.client_options.auth_scheme).to eq(:basic_auth)
+  end
+
+  it "uses request_body authentication when client_secret_post is provided only" do
+    discovery_document.merge!({ "token_endpoint_auth_methods_supported" => ["client_secret_post"] })
+    subject.discover!
+    expect(subject.options.client_options.auth_scheme).to eq(:request_body)
+  end
+
+  context 'with valid discovery document loaded' do
+    before do
+      subject.stubs(:request).returns(mock('object'))
+      subject.request.stubs(:params).returns("p" => "someallowedvalue", "somethingelse" => "notallowed")
+      subject.options.claims = '{"userinfo":{"email":null,"email_verified":null}'
+      subject.discover!
+    end
+
+    it "loads parameters correctly" do
+      expect(subject.options.client_options.site).to eq("https://id.example.com/")
+      expect(subject.options.client_options.authorize_url).to eq("https://id.example.com/authorize")
+      expect(subject.options.client_options.token_url).to eq("https://id.example.com/token")
+      expect(subject.options.client_options.userinfo_endpoint).to eq("https://id.example.com/userinfo")
+    end
+
+    describe "authorize parameters" do
+      it "passes through allowed parameters" do
+        expect(subject.authorize_params[:p]).to eq("someallowedvalue")
+        expect(subject.authorize_params[:somethingelse]).to eq(nil)
+
+        expect(subject.session["omniauth.param.p"]).to eq("someallowedvalue")
+      end
+
+      it "sets a nonce" do
+        expect((nonce = subject.authorize_params[:nonce]).size).to eq(64)
+        expect(subject.session['omniauth.nonce']).to eq(nonce)
+      end
+
+      it "passes claims through to authorize endpoint if present" do
+        expect(subject.authorize_params[:claims]).to eq('{"userinfo":{"email":null,"email_verified":null}')
+      end
+
+      it "does not pass claims if empty string" do
+        subject.options.claims = ""
+        expect(subject.authorize_params[:claims]).to eq(nil)
+      end
+    end
+
+    describe "token parameters" do
+      it "passes through parameters from authorize phase" do
+        expect(subject.authorize_params[:p]).to eq("someallowedvalue")
+        subject.stubs(:request).returns(mock())
+        subject.request.stubs(:params).returns({})
+        expect(subject.token_params[:p]).to eq("someallowedvalue")
+      end
+    end
+
+    describe "callback_phase" do
+      before do
+        auth_params = subject.authorize_params
+
+        subject.stubs(:full_host).returns("https://example.com")
+
+        subject.stubs(:request).returns(mock())
+        subject.request.stubs(:params).returns("state" => auth_params[:state], "code" => "supersecretcode")
+
+        payload = {
+          iss: "https://id.example.com/",
+          sub: "someuserid",
+          aud: "appid",
+          iat: Time.now.to_i - 30,
+          exp: Time.now.to_i + 120,
+          nonce: auth_params[:nonce],
+          name: "My Auth Token Name",
+          email: "tokenemail@example.com"
+        }
+        @token = ::JWT.encode payload, nil, 'none'
+      end
+
+      it "handles error redirects correctly" do
+        subject.stubs(:request).returns(mock())
+        subject.request.stubs(:params).returns("error" => true, "error_description" => "User forgot password")
+        subject.options.error_handler = lambda { |error, message| return "https://example.com/error_redirect" if message.include?("forgot password") }
+        expect(subject.callback_phase[0]).to eq(302)
+        expect(subject.callback_phase[1]["Location"]).to eq("https://example.com/error_redirect")
+        expect(@app_called).to eq(false)
+      end
+
+      context "with userinfo disabled" do
+        before do
+          stub_request(:post, "https://id.example.com/token").
+            with(body: hash_including("code" => "supersecretcode", "p" => "someallowedvalue")).
+            to_return(status: 200, body: {
+            "id_token": @token,
+          }.to_json, headers: { "Content-Type" => "application/json" })
+
+          subject.options.use_userinfo = false
+        end
+
+        it "fetches auth token correctly, and uses it for user info" do
+          expect(subject.callback_phase[0]).to eq(200)
+          expect(subject.uid).to eq("someuserid")
+          expect(subject.info[:name]).to eq("My Auth Token Name")
+          expect(subject.info[:email]).to eq("tokenemail@example.com")
+          expect(subject.extra[:id_token]).to eq(@token)
+          expect(@app_called).to eq(true)
+        end
+
+        it "checks the nonce" do
+          subject.session["omniauth.nonce"] = "overriddenNonce"
+          expect(subject.callback_phase[0]).to eq(302)
+          expect(@app_called).to eq(false)
+        end
+
+        it "checks the issuer" do
+          subject.options.client_id = "overriddenclientid"
+          expect(subject.callback_phase[0]).to eq(302)
+          expect(@app_called).to eq(false)
+        end
+      end
+
+      context "with userinfo enabled" do
+        before do
+          stub_request(:post, "https://id.example.com/token").
+            with(body: hash_including("code" => "supersecretcode", "p" => "someallowedvalue")).
+            to_return(status: 200, body: {
+            "access_token": "AnAccessToken",
+            "expires_in": 3600,
+            "id_token": @token,
+          }.to_json, headers: { "Content-Type" => "application/json" })
+
+          stub_request(:get, "https://id.example.com/userinfo").
+            with(headers: { 'Authorization' => 'Bearer AnAccessToken' }).
+            to_return(status: 200, body: {
+              sub: "someuserid",
+              name: "My Userinfo Name",
+              email: "userinfoemail@example.com",
+            }.to_json, headers: { "Content-Type" => "application/json" })
+        end
+
+        it "fetches credentials and auth token correctly" do
+          expect(subject.callback_phase[0]).to eq(200)
+          expect(subject.uid).to eq("someuserid")
+          expect(subject.info[:name]).to eq("My Userinfo Name")
+          expect(subject.info[:email]).to eq("userinfoemail@example.com")
+          expect(@app_called).to eq(true)
+        end
+      end
+    end
+  end
+end
